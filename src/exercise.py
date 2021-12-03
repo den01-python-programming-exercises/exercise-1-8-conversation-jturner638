7554f054b08537aa332b20b431e0c703807d60b9
Justin Turner <mrfathed@protonmail.com>
2021-12-03 13:13:17 -0600

    First Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,8 @@
 def main():
     #write your code below this line
+    message = input("Greetings! How are you?")
+    message = input("Oh, how interesting. Tell me more!")
+    print("Thanks for sharing!")
 
 if __name__ == '__main__':
     main()
